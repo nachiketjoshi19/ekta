309f98344510af54a4431dd1cd2c5a5b4c4a82a7
nachiketj199 <nachiketjoshi61@gmail.com>
2020-08-20 11:33:38 +0530

    covid19 added

--- js/script.js
+++ js/script.js
@@ -16,3 +16,32 @@
 //     document.body.scrollTop = 0; // For Safari
 //     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 // }
+
+// window.onscroll = function() { scrollFunction() };
+
+// function scrollFunction(){
+//     console.log(window.scrollY)
+//     if(window.scrollY>=500){
+//         document.getElementById('navbar').classList.add('navbar-display')
+//     }
+//     else{
+//         document.getElementById('navbar').classList.remove('navbar-display')
+
+//     }
+// }
+
+
+// detect scroll top or down
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else  {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
